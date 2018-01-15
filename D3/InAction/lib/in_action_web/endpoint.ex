9b2b818a10033bcbdc2ae39da2696977683d74b5
H.Tajima <tajihiro2@gmail.com>
2018-01-15 23:47:43 +0900

    Modified Worldcup Sample modules.

--- D3/InAction/lib/in_action_web/endpoint.ex
+++ D3/InAction/lib/in_action_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule InActionWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :in_action, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt data)
+    only: ~w(css fonts images js favicon.ico robots.txt data html)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
