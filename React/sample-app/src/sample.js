8606614660dc63cfc33e785a1f39c0b2a597c383
H.Tajima <tajihiro2@gmail.com>
2018-03-01 00:29:54 +0900

    Create React App.

---
+++ React/sample-app/src/sample.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Blowfish = () => {
+    return <div>Hello Blowfish!!</div>;
+};
+
+ReactDOM.render(
+    <div>
+        <Blowfish />
+    </div>,
+    document.getElementById('root')
+)
